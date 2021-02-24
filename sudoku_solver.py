c7a4fd06121fd736604e0112283a1aed857f916a
RafaelConejo <rafavazquez99@gmail.com>
2021-02-24 22:09:59 +0100

    Funciones definidas

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -24,10 +24,33 @@ def encontrar_vacio(tablero):
 """
     Función para validar el valor de una casilla
     :param tablero: lista de int con el tablero
-    :return: fila, columna (int, int)
+    :param posicion: (fila, columna)
+    :param valor: int
+    :return: bool
 """
+def valor_valido(tablero, posicion, valor):
+    salida = True
 
+    #compruebo la fila
+    for i in range(0, len(tablero)):
+        if(tablero[posicion[0]][i] == valor and posicion[1] != i):
+            salida = False
 
+    #compruebo columna
+    for i in range(0, len(tablero[0])):
+        if(tablero[i][posicion[1]] == valor and posicion[0] != i):
+            salida = False
+
+    #Compruebo la caja a la que pertenece
+    caja_x = posicion[1]//3
+    caja_y = posicion[0]//3
+
+    for i in range(caja_y*3, caja_y*3 + 3):
+        for j in range(caja_x*3, caja_x*3 + 3):
+            if(tablero[i][j] == valor and (i, j) != posicion):
+                salida = False
+
+    return salida
 
 
 """
@@ -44,4 +67,12 @@ def resolver(tablero):
         return True
 
     for i in range(1, 10):
-        if valor_valido()
+        if valor_valido(tablero, (fil, col), i):
+            tablero[fil][col] = i
+
+            if(resolver(tablero)):
+                return True
+
+            tablero[fil][col] = 0
+
+    return False
